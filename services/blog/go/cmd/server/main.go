93bede5e5b3cbe537ffc7daba349267b32139a1d
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro-2.local>
2025-01-18 22:25:32 +0900

    save

--- services/blog/go/cmd/server/main.go
+++ services/blog/go/cmd/server/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kelseyhightower/envconfig"
@@ -17,42 +14,6 @@ import (
 	"github.com/suzuito/sandbox3-go/services/blog/go/internal/inject"
 	"github.com/suzuito/sandbox3-go/services/blog/go/internal/web"
 )
-
-func RunServer(
-	server *http.Server,
-	logger *slog.Logger,
-) int {
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	chGracefulShutdown := make(chan error)
-	defer close(chGracefulShutdown)
-	go func() {
-		// Signalのハンドラー
-		// SIGINT,SIGTERMをキャッチした後、ctx.Doneが制御を返す
-		<-ctx.Done()
-		logger.Info("start graceful shut down")
-		ctxSignalHandler, cancel := context.WithTimeout(context.Background(), time.Second*100) // 100秒待ってもserver.Shutdown(ctx)が返ってこない場合、強制的にシャットダウンする
-		defer cancel()
-		// Graceful Shutdownをスタートする。
-		// Graceful Shutdownが成功したら、server.Shutdown(ctxSignalHandler)はnilを返す。
-		// Graceful Shutdownが失敗したら、server.Shutdown(ctxSignalHandler)は非nilを返す。
-		chGracefulShutdown <- server.Shutdown(ctxSignalHandler)
-	}()
-
-	if err := server.ListenAndServe(); err != nil {
-		if !errors.Is(err, http.ErrServerClosed) {
-			logger.Error("server.ListenAndServe() is failed", "err", err)
-			return 1
-		}
-	}
-	if err := <-chGracefulShutdown; err != nil {
-		logger.Error("graceful shut down is failed (server.Shutdown(ctx) is failed)", "err", err)
-		return 2
-	}
-	logger.Info("graceful shut down is complete")
-	return 0
-}
 
 func main() {
 	var env inject.Environment
